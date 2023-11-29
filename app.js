d9de1cc571be30447e019c32c1fe94703bbc50c2
Zconsulto <ashorbagy@zconsulto.com>
2023-11-29 23:15:22 +0200

    c

--- app.js
+++ app.js
@@ -27,3 +27,27 @@
     });
 
 })();
+
+document.addEventListener('DOMContentLoaded', function() {
+    const screen = document.getElementById('calc-screen');
+    
+    document.addEventListener('keydown', function(event) {
+        // Check if the key is a number or an allowed operator
+        if ((event.key >= '0' && event.key <= '9') || ['+', '-', '*', '/', '.'].includes(event.key)) {
+            screen.value += event.key;
+        } else if (event.key === 'Enter' || event.key === '=') {
+            // Calculate the result if Enter or = is pressed
+            try {
+                screen.value = eval(screen.value);
+            } catch (e) {
+                screen.value = 'Error';
+            }
+        } else if (event.key === 'Backspace') {
+            // Implement backspace functionality
+            screen.value = screen.value.slice(0, -1);
+        } else if (event.key === 'Escape') {
+            // Clear the screen if Esc is pressed
+            screen.value = '';
+        }
+    });
+});
